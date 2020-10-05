75b1e69299c3b330a444db526baf621b6c8591ed
marieweltm <mariewelt@gmail.com>
2020-10-05 03:22:16 +0000

    debug

--- python/gpusim_server.py
+++ python/gpusim_server.py
@@ -259,7 +259,7 @@ def parse_args():
     parser = argparse.ArgumentParser(description="Sample GPUSim Server - "
             "run an HTTP server that loads fingerprint data onto GPU and " #noqa
             "responds to queries to find most similar fingperints.") #noqa
-    parser.add_argument('--dbnames', help=".fsim files containing fingerprint "
+    parser.add_argument('dbnames', help=".fsim files containing fingerprint "
                         "data to be searched", nargs='*')
     parser.add_argument('--hostname', default="localhost",
                         help="Hostname to run on")
